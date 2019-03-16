f71dc4082b3dd3b29ccecaf8a46914064e2b511c
michaellikaiye <mye1@stuy.edu>
2019-03-15 20:23:56 -0400

    .

--- test0/dlist.py
+++ test0/dlist.py
@@ -6,12 +6,12 @@ class Node:
         self.previous = None
 class Dlist:
     def __init__(self):
-        self.top = None
+        self.top = self.end = None
 
     def insert(self, value):
         new = Node(value)
         if self.top == None:
-            self.top = new
+            self.top = self.end = new
         elif self.top.v >= new.v:
             new.next = self.top
             new.next.previous = new
@@ -25,33 +25,52 @@ class Dlist:
                 new.next.previous = new
             current.next = new
             new.previous = current
+            if current.next.next == None:
+                self.end = current.next
     def delete(self, value):
         if self.top == None:
             return False
         current = self.top
         if current.v == value:
-            if current.next == None:
-                self.top = None
+            if current == self.end:
+                self.top = self.end = None
             else:
                 self.top = self.top.next
                 self.top.prevous = None
             return True
         while current != None and current.v < value:
             current = current.next
-        if current == None or current.next > value:
+        if current == None or current.v != value:
             return False
         else:
             if current.next == None:
                 current.previous.next = None
+                self.end = current.previous
+                current = None
             else:
                 current.previous.next = current.next
                 current.next.prevous = current.prevous
+                current.next = None
+                current.previous = None
+    def printlist(self):
+        current = self.top
+        s = '['
+        while current != None:
+            s += str(current.v)
+            s += ','
+            current = current.next
+            
+        if len(s) != 1:
+            s = s[:-1]
+        s += ']'
+        return s
+            
     def tolist(self):
         if self.top == None:
             return []
         A = []
         current = self.top
-        while current != None:
+        while not(current == None):
             A.append(current.v)
             current = current.next
         B = A[::-1]
@@ -60,10 +79,18 @@ class Dlist:
         return A
 
 C = Dlist()
+print(C.end)
 C.insert(1)
 C.insert(2)
 C.insert(5)
 C.insert(0)
-print(C.delete(0))
+print(C.printlist())
+print(C.top.v)
+C.delete(5)
+print(C.printlist())
+print("end:",C.end.v)
+print(C.end.next)
 print(C.tolist())
-print(C.tolist())
+print(C.printlist())
+print(C.top)
+print(C.end)
